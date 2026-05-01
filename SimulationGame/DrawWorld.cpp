1043f06508ac472cd110362cdeb7f550c84809ae
SasakiDasiuke <sdt2495gp0006@edu.sca.ac.jp>
2026-05-01 14:40:11 +0900

    ゲーム制作環境構築

---
+++ SimulationGame/DrawWorld.cpp
@@ -0,0 +1,29 @@
+#include "DxLib.h"
+#include "../SimulationGame/Source/Screen.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);
+
+	SetGraphMode(Screen::WIDTH, Screen::HEIGHT, 32);
+	SetMainWindowText(Screen::WINDOW_NAME);
+	SetWindowSizeExtendRate(Screen::WINDOW_EXTEND);
+	ChangeWindowMode(Screen::WINDOW_MODE); // Windowモードの場合
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	while (ProcessMessage() == 0)
+	{
+		ClearDrawScreen();
+
+		ScreenFlip();
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
